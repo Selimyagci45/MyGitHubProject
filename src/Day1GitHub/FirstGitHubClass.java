f7a554d190a59a4b178f721263481c1f6c2717ac
Selimyagci45 <123612461+Selimyagci45@users.noreply.github.com>
2023-01-30 23:37:25 -0500

    Delete FirstGitHubClass.java

--- src/Day1GitHub/FirstGitHubClass.java
+++
@@ -1,8 +0,0 @@
-package Day1GitHub;
-
-public class FirstGitHubClass {
-    public static void main(String[] args) {
-        System.out.println("Hello Mr. GitHub");
-        System.out.println(1+1);
-    }
-}
