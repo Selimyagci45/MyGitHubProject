4bd0deae7a5b297fed17181248f58a1871674447
Selimyagci45 <123612461+Selimyagci45@users.noreply.github.com>
2023-01-30 23:33:04 -0500

    Update FirstGitHubClass.java
    I add Mr.

--- src/Day1GitHub/FirstGitHubClass.java
+++ src/Day1GitHub/FirstGitHubClass.java
@@ -2,7 +2,7 @@ package Day1GitHub;
 
 public class FirstGitHubClass {
     public static void main(String[] args) {
-        System.out.println("Hello GitHub");
+        System.out.println("Hello Mr. GitHub");
         System.out.println(1+1);
     }
 }
